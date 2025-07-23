ef1c47d5a53c25f4f225c2f6120c626c18c36e4d
Odoh Stanley <stanleyodoh780@gmail.com>
2025-07-23 17:23:45 +0100

    half done  test for updateDeliveryOption

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,4 @@
+import { validDeliveryOption } from "./deliveryOptions.js";
 export let cart;
 
 loadFromStorage();
@@ -93,6 +94,14 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
       matchingItem = cartItem;
     }
   });
+
+  if(!matchingItem){
+    return
+  }
+
+  if(!validDeliveryOption(deliveryOptionId)){
+    return;
+  }
 
   matchingItem.deliveryOptionId = deliveryOptionId;
   
--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addtoCart, cart, loadFromStorage, removeFromCart } from "../../data/cart.js";
+import { addtoCart, cart, loadFromStorage, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
@@ -92,5 +92,42 @@ describe('test suite: removeFromCart', () => {
     expect(cart.length).toEqual(2);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     console.log(cart);
+  })
+})
+
+describe('test suite: updateDeliveryOption()', () =>{
+  beforeEach(() => {
+    spyOn(localStorage,'setItem')
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify(   
+      [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1',
+      },
+      {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2',
+      }
+    ]);
+  })
+    loadFromStorage();
+  })
+  
+
+  it('update delivery option', () => {
+    updateDeliveryOption('e43638ce-6aa0-4b85-b27f-e1d07eb678c6','3')
+
+    expect(cart.length).toEqual(2)
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6')
+    expect(cart[0].deliveryOptionId).toEqual('3')
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1)
+  })
+
+  it("doesn't update deliveryoption",() => {
+    updateDeliveryOption('am253hs-sd23-sdo64384-4743640-3848','2');
+
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
   })
 })

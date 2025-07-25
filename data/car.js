52b88533da232d00e79da28a7a6943dbb29e6717
Odoh Stanley <stanleyodoh780@gmail.com>
2025-07-25 10:54:50 +0100

    Create a class Car which adjusts speed and closeor open trunk

---
+++ data/car.js
@@ -0,0 +1,80 @@
+class Car {
+  brand;
+  model;
+  speed = 0;
+  isTrunkOpen = true;
+
+  constructor(carDetails) {
+    this.brand = carDetails.brand;
+    this.model = carDetails.model;
+    this.speed = carDetails.speed;
+    this.isTrunkOpen =carDetails.isTrunkOpen;
+  }  
+  //displayInfo() is a method
+  displayinfo() {
+    console.log(
+      `${this.brand} ${this.model}, ${this.speed} km/h, ${this.isTrunkOpen}`
+    )
+  }
+
+  go() {
+    if(this.isTrunkOpen === true) {
+      return
+    }
+    this.speed += 5
+    if(this.speed > 200) {
+    this.speed = 200
+   }
+  }
+
+  brake() {
+    this.speed -= 5
+    if(this.speed < 0) {
+    this.speed = 0
+   }
+  }
+
+  openTrunk() {
+    if(this.speed = 0 ) {
+      this.isTrunkOpen = true;
+    }
+  }
+
+  closeTrunk() {
+    this.isTrunkOpen = false
+  }
+  
+  
+}
+
+const car1 = new Car ({
+  brand: 'Toyota',
+  model: 'Corolla',
+  speed: 10,
+  isTrunkOpen: true
+ })
+const car2 = new Car({
+  brand: 'Tesla',
+  model: 'Model 3',
+  speed: 40,
+  isTrunkOpen: false
+ })
+
+car1.displayinfo()
+car2.displayinfo()
+
+car1.go()
+car1.go()
+car1.go()
+car1.go()
+car1.brake()
+
+
+car2.brake()
+car2.brake()
+car2.go()
+car2.openTrunk()
+
+
+car1.displayinfo()
+car2.displayinfo()
